65de28cc4e8fa0a95b1f1afe826f7ef379010931
irikay <loldorolo@hotmail.com>
2018-05-01 17:33:16 +0200

    Changement pour chi2, et ajout test gap

--- Code/PiDecimalCounter.py
+++ Code/PiDecimalCounter.py
@@ -2,20 +2,27 @@ from Code import TestPi
 
 '''
 Compte le nombre de 0,1,2,3,... dans les décimale de Pi et les place dans une liste
-Le i ème chiffre se trouve dans la i ème possition dans la liste retourner
+Le nombre d'occurence du i ème chiffre se trouve dans la i ème position dans la liste retourner
 '''
 def countDigitFrequency():
-    pi_f = open("../Files/pi.txt", "r")
-    pi = ""
-    for line in pi_f:
-        pi += line.rstrip('\r \n')
-    #On enleve le '3.14' de pi
-    pi = pi[2:]
+    pi = getPiDecimalNumber()
     count = [0,0,0,0,0,0,0,0,0,0]
     for number in pi:
         number = int(number)
         count[number] = count[number] + 1
     return count
 
+'''
+Retourne toutes les decimales de pi
+'''
+def getPiDecimalNumber():
+    pi_f = open("../Files/pi.txt", "r")
+    pi = ""
+    for line in pi_f:
+        pi += line.rstrip('\r \n')
+    # On enleve le '3.14' de pi
+    return pi[2:]
+
 if __name__ == '__main__':
-    print(TestPi.chi2(countDigitFrequency()))
+    print(TestPi.chi2Pi())
+    print(TestPi.gapPi())
--- Code/TestPi.py
+++ Code/TestPi.py
@@ -1,21 +1,79 @@
 import math
 from scipy import stats
+from Code import PiDecimalCounter
+
+
+
+'''
+Test du Chi2 sur les decimal de Pi
+'''
+def chi2Pi():
+    countPi = PiDecimalCounter.countDigitFrequency()
+    #Il y a 10 chiffre dans la proba de chaque chiffre est de 10%
+    proba = 1./10
+    number = len(PiDecimalCounter.getPiDecimalNumber())
+    return chi2(countPi, proba, number)
 
 '''
 Test du Chi2, Vrai si on accepte l'hypothèse, faux sinon
 '''
-def chi2(count):
-    #La proba de chacun des intervale est de 10%, car il y a 10 intervale
-    #probability_i = 0.1
-
+def chi2(count, proba, number, alpha = 0.05):
+    pi = number*proba
     Kr = 0
 
     for i in range (0, len(count)):
         ni = count[i]
-        Kr = Kr + (math.pow((ni - 100000), 2))/100000
+        Kr = Kr + (math.pow((ni - pi), 2))/pi
 
     df = len(count) - 1
-    alpha = 0.05
     critical = stats.chi2.ppf(1 - alpha, df)
     return critical >  Kr
+
+
+def gapPi():
+    pi = PiDecimalCounter.getPiDecimalNumber()
+    numberOfClass = 60
+    for i in range(0, 1):
+        gapTest(pi, i, numberOfClass, alpha = 0.05)
+
+
+def gapTest(sequence, number, numberOfClasses, alpha):
+    classes = [0] * numberOfClasses
+    gap = 0
+    seen = False
+    for n in range(0, len(sequence)):
+        num = int(sequence[n])
+        if num == number:
+            if seen and gap < numberOfClasses :
+                classes[gap] = classes[gap] + 1
+                gap = 0
+            else:
+                gap = 0
+                seen = True
+        else:
+            gap += 1
+
+    for n in range(0, numberOfClasses):
+        #print(n)
+        theoricalN = getTheoricalProbaGap(n)
+        #print(theoricalN)
+
+def getTheoricalProbaGap(gap):
+    return (0.9**gap)*(0.1)
+
+def poker():
+    r = 10
+    k = 5
+    sn = sterling_number(r, k)
+
+
+def sterling_number(r, k):
+    if (k == 1) or (k == r):
+        return 1
+    else:
+        return sterling_number(r-1, k-1) + (r * sterling_number(r, k-1))
+
+
+
+
 
